07a39d38e8491f06f056b57abfa4c0d9d500d7a3
jimee <jimee@votez-cthulhu.net>
2021-12-09 06:42:41 +0100

    bugfix

--- 9_smoke_part1.py
+++ 9_smoke_part1.py
@@ -13,10 +13,10 @@ for y in range(Y):
     for x in range(X):
         v=data[y][x]
         m=True
-        if m and x>0 and data[y][x-1] < v: m=False
-        if m and y>0 and data[y-1][x] < v: m=False
-        if m and x<X-1 and data[y][x+1] < v: m=False
-        if m and y<Y-1 and data[y+1][x] < v: m=False
+        if m and x>0 and data[y][x-1] <= v: m=False
+        if m and y>0 and data[y-1][x] <= v: m=False
+        if m and x<X-1 and data[y][x+1] <= v: m=False
+        if m and y<Y-1 and data[y+1][x] <= v: m=False
         if m: count += v + 1
 
 print(count)
