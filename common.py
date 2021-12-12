66a8607baaf25b3a7eded23715209f370d60ce8c
jimee <jimee@votez-cthulhu.net>
2021-12-12 12:03:59 +0100

    caching input

--- common.py
+++ common.py
@@ -1,7 +1,15 @@
 import requests
+import os
 
 with open("session.txt","r") as f: sess=f.read()
 
 def read_input(day):
-    r = requests.get(f"https://adventofcode.com/2021/day/{day}/input",cookies={'session':sess})
-    return r.content.decode().splitlines()
+    path = f"tmp/{day}.txt"
+    if not os.path.isfile(path):
+        print("caching...")
+        r = requests.get(f"https://adventofcode.com/2021/day/{day}/input",cookies={'session':sess})
+        data = r.content.decode()
+        with open(path,"w") as f: f.write(data)
+    else:
+        with open(path,"r") as f: data = f.read()
+    return data.splitlines()
