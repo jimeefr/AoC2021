c38c6ffb6658f059da45b9808afabd0a9a042ba1
jimee <jimee@votez-cthulhu.net>
2021-12-17 08:12:15 +0100

    cleanup and optimize

--- 17_probe_part1.py
+++ 17_probe_part1.py
@@ -6,12 +6,7 @@ from collections import defaultdict
 data = read_input(17)
 
 data=data[0].split(',')
-targetx = list(map(int,data[0].split('=')[1].split('..')))
 targety = list(map(int,data[1].split('=')[1].split('..')))
 
-print(targetx,targety)
-
 vy=-targety[0]-1
-print(vy)
-
 print((vy*(vy+1))//2)
--- 17_probe_part2.py
+++ 17_probe_part2.py
@@ -2,14 +2,13 @@
 
 from common import *
 from collections import defaultdict
+from math import sqrt
 
 data = read_input(17)
 
 data=data[0].split(',')
 targetx = list(map(int,data[0].split('=')[1].split('..')))
 targety = list(map(int,data[1].split('=')[1].split('..')))
-
-print(targetx,targety)
 
 def vcheck(vx,vy):
     x,y=0,0
@@ -22,7 +21,7 @@ def vcheck(vx,vy):
     return False
 
 count=0
-for vx in range(targetx[1]+1):
+for vx in range(int(sqrt(targetx[0]*2)),targetx[1]+1):
     for vy in range(targety[0]-1,-targety[0]+1):
         if vcheck(vx,vy): count+=1
 print(count)
